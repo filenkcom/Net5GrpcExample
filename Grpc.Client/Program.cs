8703832ed9b613be744ca1845c243b1f31e2572d
filenkcom <filenkcom@gmail.com>
2021-05-10 05:02:58 +0300

    initial

---
+++ Grpc.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+
+namespace Grpc.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloRequest request = new HelloRequest()
+            {
+                Name = "World",
+            };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            try
+            {
+                var reply = client.SayHello(request);
+                Console.WriteLine(reply.Message);
+            }
+            catch (RpcException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
